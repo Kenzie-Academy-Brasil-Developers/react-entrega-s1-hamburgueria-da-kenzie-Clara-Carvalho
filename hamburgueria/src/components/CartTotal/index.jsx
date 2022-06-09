0c544444e12524063c9ee2906fad5c58e65e18cf
Clara Cavalcantee <claracarvc@gmail.com>
2022-06-09 16:36:40 -0300

    bug fix

--- hamburgueria/src/components/CartTotal/index.jsx
+++ hamburgueria/src/components/CartTotal/index.jsx
@@ -3,7 +3,7 @@ export default function CartTotal({currentSale, setCurrentSale}){
         <div className="cart--total">
             <div className="cart--total--title">
                 <h5>Valor Total:</h5>
-                <span>{currentSale.reduce((value, product) => (value + product.price), 0)}</span>
+                <span>{currentSale.reduce((value, product) => (value + product.price), 0).toFixed(2)}</span>
             </div>
             <button className="removeAll" onClick={() => setCurrentSale([])}>Remover Todos</button>
             
--- hamburgueria/src/components/Search/index.jsx
+++ hamburgueria/src/components/Search/index.jsx
@@ -13,12 +13,12 @@ const handleSearchChange = (event) => {
 const handleOnSubmit = (event) => {
     event.preventDefault();
     
-    setProducts( products.filter(product =>(product.name.toLowerCase().includes(search.toLowerCase()) === true )))
+    setProducts( products.filter(product =>(product.name.toLowerCase().includes(search.toLowerCase()) === true || product.category.toLowerCase().includes(search.toLowerCase()) )))
 }
 
     return (
         <div className="search">
-            <input className="form-control" type="text" onChange={(event) => handleSearchChange(event)}/>
+            <input className="form-control" type="text" onChange={(event) => handleSearchChange(event)} placeholder="Digite a Pesquisa"/>
             <button className="btn--search"type="submit" onClick={(event) => handleOnSubmit(event)}>Pesquisar</button>
         </div>
     )
